2f67dc70bc8bf8d44b75722508df0f5d6f0ec7de
valderfarkill <bigzam.3@outlook.com>
2023-09-23 02:46:01 +0200

    aggiornamento portfolio

--- pages/3_📧_Contatti.py
+++ pages/3_📧_Contatti.py
@@ -11,18 +11,10 @@ st.sidebar.success("Select a Project above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    📧 [Email](bigzam.3@outlook.com)
+
+    👥 [Linkedin](https://www.linkedin.com/in/emanuele-tonti/)
+
+    🗺️ [Instagram](https://www.instagram.com/_goldenmanu_/)
 """
 )
