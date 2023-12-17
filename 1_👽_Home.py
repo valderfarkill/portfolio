372f113bbc2cfc56af0bbb9efd9d27f5f058e890
unknown <bigzam.3@outlook.com>
2023-12-17 15:01:49 +0100

    portfoliov2

--- 1_👽_Home.py
+++ 1_👽_Home.py
@@ -13,7 +13,7 @@ st.subheader("Data Analyst/Designer Product Manager")
 st.write("👋🏻 Ciao, Sono Manu. Designer e Data Analyst, sto cercando attivamente lavoro.Parlo Inglese e Italiano")
 st.write("🏋🏻 Hobby: Mi piace l'informatica, leggere, i videogiochi, e soprattuto mangiare!")
 st.write("👨🏼‍💻 Interessi accademici: Design, Data Science, Cybersecurity")
-st.write("💭 Posizioni ideali: Data Analyst, DBA, Product Manager, Designer, CYbersecurity Analyst")
+st.write("💭 Posizioni ideali: Data Analyst, DBA, Product Manager, Designer, Cybersecurity Analyst")
 
 pdfFileObj = open('pdfs/Emanuele_Tonti_CV_.pdf', 'rb')
 st.sidebar.download_button('📄Scarica CV',pdfFileObj,file_name='Emanuele_Tonti_CV_.pdf',mime='pdf')
